305851f8c56cd2c0da186b0d6a561558558c0c4f
Yanina Trekhleb <yanisavchenko@gmail.com>
2020-04-09 19:25:36 +0200

    Added new images.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import {activities} from './activitiesList';
+
+const getRandomActivity = () => {
+  const randomActivityIndex = Math.floor(Math.random() * activities.list.length);
+  const activity = activities.list[randomActivityIndex];
+  return activity;
+};
+
+const getActivityVideo = (activity) => {
+  if (!activity) {
+    return null;
+  } 
+  if (!activity.media || activity.media.length === 0) {
+    return null;
+  }
+  return activity.media.filter((media) => {
+    return media.type.name === 'video';
+  });
+};
 
 function App() {
+  const [randomActivity, setRandomActivity] = useState(null);
+
+  const onNextActivity = () => {
+    const activity = getRandomActivity();
+    setRandomActivity(activity);
+  };
+
+  useEffect(() => {
+    const activity = getRandomActivity();
+    setRandomActivity(activity);
+  }, []);
+
+  if (!randomActivity) {
+    return (<div>Loading...</div>);
+  }
+
+  const youtubeVideos = getActivityVideo(randomActivity);
+  let youtubeVideoElement = null;
+  if (youtubeVideos) {
+    youtubeVideoElement = (
+      <iframe 
+        width="560" 
+        height="315" 
+        src={youtubeVideos[0].link} 
+        frameBorder="0" 
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+        allowFullScreen>
+      </iframe>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <p>{randomActivity.title}</p>
+      {youtubeVideoElement}
+      <button onClick={onNextActivity}>Next activity</button>
     </div>
   );
 }
